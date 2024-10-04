9738ba21811978721a60b360aa0dc88de6cd4207
Sean Graham <seangraham006@gmail.com>
2024-10-04 13:22:39 +0000

    used multer to read file metadata

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
-
+const multer = require('multer');
+const upload = multer();
+require('dotenv').config();
 var app = express();
 
 app.use(cors());
@@ -11,7 +12,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req,res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+})
 
 
 const port = process.env.PORT || 3000;
